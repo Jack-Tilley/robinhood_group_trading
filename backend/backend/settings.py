fcb22ac958c04d0842e91d4cf0e4504d50c3f01d
Jack-Tilley <tilley.e.jack@gmail.com>
2021-02-10 18:35:57 -0800

    added frontend pages and config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -155,6 +155,9 @@ DJOSER = {
 }
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
-  return <div>Hello world</div>;
-}
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ResetPassword from "./pages/ResetPassword";
+import ResetPasswordConfirm from "./pages/ResetPasswordConfirm";
+import Activate from "./pages/Activate";
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/reset-password" component={ResetPassword} />
+        <Route
+          exact
+          path="/password/reset/confirm/:uid/:token"
+          component={ResetPasswordConfirm}
+        />
+        <Route exact path="/activate/:uid/:token" component={Activate} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;

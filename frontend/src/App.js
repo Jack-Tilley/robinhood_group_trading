bf360214ab6f07edb56cd6abca510d044a1112f6
Jack-Tilley <tilley.e.jack@gmail.com>
2021-02-13 13:36:51 -0800

    added login functionality with authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -9,22 +8,27 @@ import ResetPassword from "./pages/ResetPassword";
 import ResetPasswordConfirm from "./pages/ResetPasswordConfirm";
 import Activate from "./pages/Activate";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/reset-password" component={ResetPassword} />
-        <Route
-          exact
-          path="/password/reset/confirm/:uid/:token"
-          component={ResetPasswordConfirm}
-        />
-        <Route exact path="/activate/:uid/:token" component={Activate} />
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/reset-password" component={ResetPassword} />
+          <Route
+            exact
+            path="/password/reset/confirm/:uid/:token"
+            component={ResetPasswordConfirm}
+          />
+          <Route exact path="/activate/:uid/:token" component={Activate} />
+        </Switch>
+      </Router>
+    </Provider>
   );
 };
 
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { login } from "../actions/auth";
 
-const Login = () => {
-  return <div>Login</div>;
+const Login = ({ login }) => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const { email, password } = formData;
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+    // login(email, password)
+  };
+  // is user auth?
+  // then redirect to homepage
+  return (
+    <div className="container mt-5">
+      <h1>Sign in</h1>
+      <p>Sign into your account</p>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            className="form-control"
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="password"
+            name="password"
+            value={password}
+            className="form-control"
+            onChange={(e) => onChange(e)}
+            minLength="6"
+            required
+          />
+        </div>
+        <button className="btn btn-primary" type="submit">
+          Login
+        </button>
+      </form>
+      <p>
+        Don't have an account? <Link to="/signup">Sign up</Link>
+      </p>
+      <p className="mt-3">
+        Forgot your Password? <Link to="/reset-password">Reset Password</Link>
+      </p>
+    </div>
+  );
 };
 
-export default Login;
+// const mapStateToProps = (state) => ({
+//   // is authenticated?
+// });
+
+export default connect(null, { login })(Login);

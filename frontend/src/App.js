f2428dad7fc9a24d9e8e7d8a37f1f63f61c53e08
Jack-Tilley <tilley.e.jack@gmail.com>
2021-02-22 16:49:20 -0800

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,15 +10,34 @@ import ResetPasswordConfirm from "./pages/ResetPasswordConfirm";
 // import Facebook from "./pages/Facebook";
 // import Google from "./pages/Google";
 
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
+import blue from "@material-ui/core/colors/blue";
+import red from "@material-ui/core/colors/red";
+
 import { Provider } from "react-redux";
 import store from "./store";
 
 import Layout from "./hocs/Layout";
+import Appbar from "./components/Appbar";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: blue[200],
+    },
+    secondary: {
+      main: red[500],
+    },
+  },
+});
 
 const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Layout>
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <Router>
+        <Appbar />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
@@ -33,9 +52,28 @@ const App = () => (
           />
           <Route exact path="/activate/:uid/:token" component={Activate} />
         </Switch>
-      </Layout>
-    </Router>
-  </Provider>
+      </Router>
+    </Provider>
+  </ThemeProvider>
 );
 
 export default App;
+
+// {/* <Router>
+//   <Layout>
+//     <Switch>
+//       <Route exact path="/" component={Home} />
+//       <Route exact path="/login" component={Login} />
+//       <Route exact path="/signup" component={Signup} />
+//       {/* <Route exact path="/facebook" component={Facebook} />
+//           <Route exact path="/google" component={Google} /> */}
+//       <Route exact path="/reset-password" component={ResetPassword} />
+//       <Route
+//         exact
+//         path="/password/reset/confirm/:uid/:token"
+//         component={ResetPasswordConfirm}
+//       />
+//       <Route exact path="/activate/:uid/:token" component={Activate} />
+//     </Switch>
+//   </Layout>
+// </Router>; */}
